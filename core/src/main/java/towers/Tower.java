81585895916b253db171edc1b1333d0548a92e8d
sina shariati <sinashariati@sinas-MacBook-Pro.local>
2021-07-09 19:23:39 +0430

    Refactor abstract tower class to just shoot target
    the shoot method before had a shoot method that in it, a timer for
    one seconds had to gone by and then it will reduce the health of the
    attack target, but the shoot target should not be responsible for
    doing the counter, the client code must count the timer and then
    call this method. so refactored the method to just reduce the target
    health when called by the damage count.

--- core/src/main/java/towers/Tower.java
+++ core/src/main/java/towers/Tower.java
@@ -3,8 +3,6 @@ package towers;
 import cards.utils.AttackAble;
 import user.UserLevelEnum;
 
-import java.util.Timer;
-import java.util.TimerTask;
 import java.util.UUID;
 
 /**
@@ -135,13 +133,6 @@ public abstract class Tower {
      * Shoot the target and reduce it's health
      */
     public void shoot() {
-        new Timer().schedule(
-                new TimerTask() {
-                    @Override
-                    public void run() {
-                        target.reduceHealth(damage);
-                    }
-                }, 1000
-        );
+        this.target.reduceHealth(this.damage);
     }
 }
