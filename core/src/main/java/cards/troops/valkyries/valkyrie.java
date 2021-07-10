0a9ca0bc540f66ed5fb05e0dfda0f8929cf90ef0
sina shariati <sinashariati@sinas-MacBook-Pro.local>
2021-07-10 18:29:36 +0430

    Add ability for Valkyrie to know self and attack type

--- core/src/main/java/cards/troops/valkyries/valkyrie.java
+++ core/src/main/java/cards/troops/valkyries/valkyrie.java
@@ -3,6 +3,7 @@ package cards.troops.valkyries;
 import cards.troops.Troop;
 import cards.utils.MovementSpeedEnum;
 import cards.utils.Position;
+import cards.utils.TroopTypeEnum;
 import user.User;
 
 import java.util.UUID;
@@ -19,6 +20,18 @@ public class valkyrie extends Troop {
      * @param damage   the damage
      */
     public valkyrie(UUID id, User owner, Position position, int HP, int damage) {
-        super(id, 4, owner, position, HP, damage, MovementSpeedEnum.MEDIUM, true, 1, 1.5);
+        super(id,
+                4,
+                owner,
+                position,
+                HP,
+                damage,
+                MovementSpeedEnum.MEDIUM,
+                true,
+                1,
+                1.5,
+                TroopTypeEnum.GROUND,
+                TroopTypeEnum.GROUND
+        );
     }
 }
