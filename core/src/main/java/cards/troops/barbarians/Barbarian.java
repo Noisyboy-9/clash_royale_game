8bbb34961d000363e61a2fbc6ff8abe869059310
sina shariati <sinashariati@sinas-MacBook-Pro.local>
2021-07-10 15:19:50 +0430

    Add javadoc to Barbarian classs

--- core/src/main/java/cards/troops/barbarians/Barbarian.java
+++ core/src/main/java/cards/troops/barbarians/Barbarian.java
@@ -3,7 +3,16 @@ package cards.troops.barbarians;
 import cards.troops.Troop;
 import cards.utils.MovementSpeedEnum;
 
+/**
+ * The type Barbarian.
+ */
 public class Barbarian extends Troop {
+    /**
+     * Instantiates a new Barbarian.
+     *
+     * @param HP     the hp
+     * @param damage the damage
+     */
     public Barbarian(int HP, int damage) {
         super(HP, damage, MovementSpeedEnum.MEDIUM, 1, 1);
     }
