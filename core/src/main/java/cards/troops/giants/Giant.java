dd0aef9877ccb4e65d31468862d9d2f336ef91c8
sina shariati <sinashariati@sinas-MacBook-Pro.local>
2021-07-10 18:29:22 +0430

    Add ability for Giant to know self and attack type

--- core/src/main/java/cards/troops/giants/Giant.java
+++ core/src/main/java/cards/troops/giants/Giant.java
@@ -3,6 +3,7 @@ package cards.troops.giants;
 import cards.troops.Troop;
 import cards.utils.MovementSpeedEnum;
 import cards.utils.Position;
+import cards.utils.TroopTypeEnum;
 import user.User;
 
 import java.util.UUID;
@@ -22,6 +23,18 @@ public class Giant extends Troop {
      * @param damage   the damage
      */
     public Giant(UUID id, User owner, Position position, int HP, int damage) {
-        super(id, 5, owner, position, HP, damage, MovementSpeedEnum.SLOW, false, 1, 1.5);
+        super(id,
+                5,
+                owner,
+                position,
+                HP,
+                damage,
+                MovementSpeedEnum.SLOW,
+                false,
+                1,
+                1.5,
+                TroopTypeEnum.GROUND,
+                TroopTypeEnum.GROUND
+        );
     }
 }
