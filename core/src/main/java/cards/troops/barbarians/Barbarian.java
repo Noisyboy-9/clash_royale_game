438c26b7603ef84d3ba6da4627d8734f5755e595
sina shariati <sinashariati@sinas-MacBook-Pro.local>
2021-07-10 18:24:54 +0430

    Add ability for barberian to know self and attack type

--- core/src/main/java/cards/troops/barbarians/Barbarian.java
+++ core/src/main/java/cards/troops/barbarians/Barbarian.java
@@ -3,6 +3,7 @@ package cards.troops.barbarians;
 import cards.troops.Troop;
 import cards.utils.MovementSpeedEnum;
 import cards.utils.Position;
+import cards.utils.TroopTypeEnum;
 import user.User;
 
 import java.util.UUID;
@@ -21,6 +22,18 @@ public class Barbarian extends Troop {
      * @param damage   the damage
      */
     public Barbarian(UUID id, User owner, Position position, int HP, int damage) {
-        super(id, 5, owner, position, HP, damage, MovementSpeedEnum.MEDIUM, false, 1, 1.5);
+        super(id,
+                5,
+                owner,
+                position,
+                HP,
+                damage,
+                MovementSpeedEnum.MEDIUM,
+                false,
+                1,
+                1.5,
+                TroopTypeEnum.GROUND,
+                TroopTypeEnum.GROUND
+        );
     }
 }
