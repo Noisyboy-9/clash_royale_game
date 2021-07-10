d2478a77d42d921810aedf1c048041364725a452
sina shariati <sinashariati@sinas-MacBook-Pro.local>
2021-07-10 18:27:47 +0430

    Add ability for pekka to know self and attack type

--- core/src/main/java/cards/troops/pekkas/MiniPekka.java
+++ core/src/main/java/cards/troops/pekkas/MiniPekka.java
@@ -3,6 +3,7 @@ package cards.troops.pekkas;
 import cards.troops.Troop;
 import cards.utils.MovementSpeedEnum;
 import cards.utils.Position;
+import cards.utils.TroopTypeEnum;
 import user.User;
 
 import java.util.UUID;
@@ -18,6 +19,18 @@ public class MiniPekka extends Troop {
      * @param damage   the damage
      */
     public MiniPekka(UUID id, User owner, Position position, int HP, int damage) {
-        super(id, 4, owner, position, HP, damage, MovementSpeedEnum.FAST, false, 1, 1.8);
+        super(id,
+                4,
+                owner,
+                position,
+                HP,
+                damage,
+                MovementSpeedEnum.FAST,
+                false,
+                1,
+                1.8,
+                TroopTypeEnum.GROUND,
+                TroopTypeEnum.GROUND
+        );
     }
 }
