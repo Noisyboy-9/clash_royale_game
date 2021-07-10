ef3ff4a5028c1eaa32ab7dcc55d97ed4d6af63fc
sina shariati <sinashariati@sinas-MacBook-Pro.local>
2021-07-10 18:25:30 +0430

    Add ability for archer to know self and attack type

--- core/src/main/java/cards/troops/archers/Archer.java
+++ core/src/main/java/cards/troops/archers/Archer.java
@@ -3,6 +3,7 @@ package cards.troops.archers;
 import cards.troops.Troop;
 import cards.utils.MovementSpeedEnum;
 import cards.utils.Position;
+import cards.utils.TroopTypeEnum;
 import user.User;
 
 import java.util.UUID;
@@ -21,6 +22,18 @@ public class Archer extends Troop {
      * @param damage   the damage
      */
     public Archer(UUID id, User owner, Position position, int HP, int damage) {
-        super(id, 3, owner, position, HP, damage, MovementSpeedEnum.MEDIUM, false, 5, 1.2);
+        super(id,
+                3,
+                owner,
+                position,
+                HP,
+                damage,
+                MovementSpeedEnum.MEDIUM,
+                false,
+                5,
+                1.2,
+                TroopTypeEnum.GROUND,
+                TroopTypeEnum.AIR_GROUND
+        );
     }
 }
