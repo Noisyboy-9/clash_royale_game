95daa9d82e7f71f51b2e9410abf98a84ac71c109
sina shariati <sinashariati@sinas-MacBook-Pro.local>
2021-07-10 16:00:07 +0430

    Finish Giant Troop

---
+++ core/src/main/java/cards/troops/giants/Giant.java
@@ -0,0 +1,27 @@
+package cards.troops.giants;
+
+import cards.troops.Troop;
+import cards.utils.MovementSpeedEnum;
+import cards.utils.Position;
+import user.User;
+
+import java.util.UUID;
+
+/**
+ * The type Giant.
+ */
+public class Giant extends Troop {
+
+    /**
+     * Instantiates a new Giant.
+     *
+     * @param id       the id
+     * @param owner    the owner
+     * @param position the position
+     * @param HP       the hp
+     * @param damage   the damage
+     */
+    public Giant(UUID id, User owner, Position position, int HP, int damage) {
+        super(id, 5, owner, position, HP, damage, MovementSpeedEnum.SLOW, false, 1, 1.5);
+    }
+}
