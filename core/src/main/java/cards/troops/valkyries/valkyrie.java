65df8508a3e18825b699e59cceb77b4eace16eab
sina shariati <sinashariati@sinas-MacBook-Pro.local>
2021-07-10 16:02:10 +0430

    Finish Valkyrie Troop

---
+++ core/src/main/java/cards/troops/valkyries/valkyrie.java
@@ -0,0 +1,24 @@
+package cards.troops.valkyries;
+
+import cards.troops.Troop;
+import cards.utils.MovementSpeedEnum;
+import cards.utils.Position;
+import user.User;
+
+import java.util.UUID;
+
+public class valkyrie extends Troop {
+
+    /**
+     * Instantiates a new Card.
+     *
+     * @param id       the id
+     * @param owner    the owner
+     * @param position the position
+     * @param HP       the hp
+     * @param damage   the damage
+     */
+    public valkyrie(UUID id, User owner, Position position, int HP, int damage) {
+        super(id, 4, owner, position, HP, damage, MovementSpeedEnum.MEDIUM, true, 1, 1.5);
+    }
+}
