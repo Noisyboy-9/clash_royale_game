1f45cfbbc900c912758fcce4a128c9f3f62e73b0
sina shariati <sinashariati@sinas-MacBook-Pro.local>
2021-07-10 18:26:24 +0430

    Add ability for baby dragon to know self and attack type

--- core/src/main/java/cards/troops/dragons/BabyDragon.java
+++ core/src/main/java/cards/troops/dragons/BabyDragon.java
@@ -3,6 +3,7 @@ package cards.troops.dragons;
 import cards.troops.Troop;
 import cards.utils.MovementSpeedEnum;
 import cards.utils.Position;
+import cards.utils.TroopTypeEnum;
 import user.User;
 
 import java.util.UUID;
@@ -19,6 +20,18 @@ public class BabyDragon extends Troop {
      * @param damage   the damage
      */
     public BabyDragon(UUID id, User owner, Position position, int HP, int damage) {
-        super(id, 4, owner, position, HP, damage, MovementSpeedEnum.FAST, true, 3, 1.8);
+        super(id,
+                4,
+                owner,
+                position,
+                HP,
+                damage,
+                MovementSpeedEnum.FAST,
+                true,
+                3,
+                1.8,
+                TroopTypeEnum.AIR,
+                TroopTypeEnum.AIR_GROUND
+        );
     }
 }
