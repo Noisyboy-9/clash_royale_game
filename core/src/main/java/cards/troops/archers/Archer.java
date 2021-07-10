146e6d6a2fd4f41ff89d0ce33ee886b559fa886f
sina shariati <sinashariati@sinas-MacBook-Pro.local>
2021-07-10 15:26:28 +0430

    Add Javadoc to Archer class

--- core/src/main/java/cards/troops/archers/Archer.java
+++ core/src/main/java/cards/troops/archers/Archer.java
@@ -3,7 +3,16 @@ package cards.troops.archers;
 import cards.troops.Troop;
 import cards.utils.MovementSpeedEnum;
 
+/**
+ * The type Archer.
+ */
 public class Archer extends Troop {
+    /**
+     * Instantiates a new Archer.
+     *
+     * @param HP     the hp
+     * @param damage the damage
+     */
     public Archer(int HP, int damage) {
         super(HP, damage, MovementSpeedEnum.FAST, 1, 1, 1.2);
     }
