c4a8cf7bdb9854e9f3cc49619b46193e1a8c918b
sina shariati <sinashariati@sinas-MacBook-Pro.local>
2021-07-12 17:21:27 +0430

    Finished GameDurationFinishedCommand class

--- core/src/main/java/commands/gameStateCommands/gameTime/GameDurationFinished.java
+++ core/src/main/java/commands/gameStateCommands/gameTime/GameDurationFinished.java
@@ -6,9 +6,9 @@ import user.User;
 import java.util.ArrayList;
 
 /**
- * The type Game finished command.
+ * The type Game duration finished.
  */
-public class GameFinishedCommand extends GameTimeCommand {
+public class GameDurationFinished extends GameTimeCommand {
     private final ArrayList<User> winners;
     private final ArrayList<User> losers;
 
@@ -17,22 +17,12 @@ public class GameFinishedCommand extends GameTimeCommand {
      *
      * @param gameType the game type
      */
-    public GameFinishedCommand(GameTypeEnum gameType, ArrayList<User> winners, ArrayList<User> losers) {
+    public GameDurationFinished(GameTypeEnum gameType, ArrayList<User> winners, ArrayList<User> losers) {
         super(gameType);
         this.winners = winners;
         this.losers = losers;
     }
 
-
-    /**
-     * Is winner boolean.
-     *
-     * @param user the user
-     * @return the boolean
-     */
-    public boolean isWinner(User user) {
-        return this.winners.contains(user);
-    }
 
     /**
      * Is loser boolean.
@@ -42,5 +32,15 @@ public class GameFinishedCommand extends GameTimeCommand {
      */
     public boolean isLoser(User user) {
         return this.losers.contains(user);
+    }
+
+    /**
+     * Is winner boolean.
+     *
+     * @param user the user
+     * @return the boolean
+     */
+    public boolean isWinner(User user) {
+        return this.winners.contains(user);
     }
 }
