f08e26dc3c04512b10a0183d391fc29caa0d6b60
sina shariati <sinashariati@sinas-MacBook-Pro.local>
2021-07-12 16:53:36 +0430

    Add Login Command

---
+++ core/src/main/java/commands/Authentication/AuthenticationCommand.java
@@ -0,0 +1,5 @@
+package commands.Authentication;
+
+public class AuthenticationCommand {
+
+}
---
+++ core/src/main/java/commands/Authentication/login/LoginCommand.java
@@ -0,0 +1,19 @@
+package commands.Authentication.login;
+
+public class LoginCommand {
+    private final String email;
+    private final String password;
+
+    public LoginCommand(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
