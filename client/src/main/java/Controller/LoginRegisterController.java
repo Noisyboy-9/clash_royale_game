2279b2c2b165f85d46f950baaa803b1a008e6cc6
nejadipour <nejadipour@gmail.com>
2021-07-13 14:29:14 +0430

    feat: login register controller added

---
+++ client/src/main/java/Controller/LoginRegisterController.java
@@ -0,0 +1,63 @@
+package Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
+public class LoginRegisterController
+{
+    @FXML
+    private ImageView exitButton;
+
+    @FXML
+    private ImageView changeButton;
+
+    @FXML
+    private Text changeButtonText;
+
+    @FXML
+    private Text exitButtonText;
+
+    @FXML
+    private ImageView mainButton;
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private TextField passwordField;
+
+    @FXML
+    private Text mainButtonText;
+
+    @FXML
+    private Text message;
+
+    @FXML
+    void changeButtonHandler(MouseEvent event)
+    {
+        String temp = mainButtonText.getText();
+        mainButtonText.setText(changeButtonText.getText());
+        changeButtonText.setText(temp);
+
+    }
+
+    @FXML
+    void exitHandler(MouseEvent event)
+    {
+        System.exit(0);
+
+    }
+
+    @FXML
+    void mainButtonHandler(MouseEvent event)
+    {
+        System.out.println(usernameField.getText());
+        System.out.println(passwordField.getText());
+
+    }
+
+
+}
