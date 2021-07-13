5aeb8744d0820a62f79b09e244ffef8c4e3ba7a0
sina shariati <sinashariati@sinas-MacBook-Pro.local>
2021-07-13 14:46:39 +0430

    Finish PlayerWorker class

---
+++ server/src/main/java/players/PlayerWorker.java
@@ -0,0 +1,68 @@
+package players;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+/**
+ * The type Player worker that gives client access to the server.
+ * by this class the server can communicate data with client.
+ */
+public class PlayerWorker {
+    private final ObjectOutputStream response;
+    private final ObjectInputStream request;
+    private final Socket socket;
+    private final Player player;
+
+    /**
+     * Instantiates a new Player worker.
+     *
+     * @param response the response
+     * @param request  the request
+     * @param socket   the socket
+     * @param player   the player
+     */
+    public PlayerWorker(ObjectOutputStream response, ObjectInputStream request, Socket socket, Player player) {
+        this.response = response;
+        this.request = request;
+        this.socket = socket;
+        this.player = player;
+
+    }
+
+    /**
+     * Gets response.
+     *
+     * @return the response
+     */
+    public ObjectOutputStream getResponse() {
+        return response;
+    }
+
+    /**
+     * Gets request.
+     *
+     * @return the request
+     */
+    public ObjectInputStream getRequest() {
+        return request;
+    }
+
+    /**
+     * Gets socket.
+     *
+     * @return the socket
+     */
+    public Socket getSocket() {
+        return socket;
+    }
+
+    /**
+     * Gets player.
+     *
+     * @return the player
+     */
+    public Player getPlayer() {
+        return player;
+    }
+}
