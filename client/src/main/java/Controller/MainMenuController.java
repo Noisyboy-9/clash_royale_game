2836a39665da60640bd18265ba84b2b95b5041bc
nejadipour <nejadipour@gmail.com>
2021-07-14 08:25:01 +0430

    feat: username and level field fxid added

--- client/src/main/java/Controller/MainMenuController.java
+++ client/src/main/java/Controller/MainMenuController.java
@@ -2,9 +2,19 @@ package Controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
 
 public class MainMenuController extends MenuController
 {
+    @FXML
+    private Text levelField;
+
+    @FXML
+    private Text usernameField;
+
+    // TODO: ۱۴/۰۷/۲۰۲۱ a method is needed to update level and username fields
+
+    
     @FXML
     void twoPlayerMode(MouseEvent event)
     {
