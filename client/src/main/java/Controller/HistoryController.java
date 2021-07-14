f2095ed95c276fd88b83981956b136c9923ef927
nejadipour <nejadipour@gmail.com>
2021-07-14 16:47:42 +0430

    feat: fxid and methods added

--- client/src/main/java/Controller/HistoryController.java
+++ client/src/main/java/Controller/HistoryController.java
@@ -1,6 +1,58 @@
 package Controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
 public class HistoryController extends MenuController
 {
+    @FXML
+    private Text playerScoreTopBox;
+
+    @FXML
+    private Text opponentScoreTopBox;
+
+    @FXML
+    private Text playerScoreBottomBox;
+
+    @FXML
+    private Text opponentScoreBottomBox;
+
+    @FXML
+    private Text playerNameTopBox;
+
+    @FXML
+    private Text playerTeammateTopBox;
+
+    @FXML
+    private Text opponentNameTopBox;
+
+    @FXML
+    private Text opponentTeammateTopBox;
+
+    @FXML
+    private Text playerNameBottomBox;
+
+    @FXML
+    private Text playerTeammateBottomBox;
+
+    @FXML
+    private Text opponentNameBottomBox;
+
+    @FXML
+    private Text opponentTeammateBottomBox;
+
+
+    @FXML
+    void moveDown(MouseEvent event)
+    {
+
+    }
+
+    @FXML
+    void moveUp(MouseEvent event)
+    {
+
+    }
 
 }
