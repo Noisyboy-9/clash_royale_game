6bce3cb175468445fd0bddedcb954edf591addaa
sina shariati <sinashariati@sinas-MacBook-Pro.local>
2021-07-15 17:47:06 +0430

    Removed all ingoreable files

--- client/src/main/java/Controller/Controller.java
+++ client/src/main/java/Controller/Controller.java
@@ -7,5 +7,4 @@ public interface Controller
     String PATH = Controller.class.getProtectionDomain().getCodeSource().getLocation().getPath().replace("target/classes/", "src/main/java/View/").substring(1);
     Stage STAGE = new Stage();
     SceneController SCENE_CONTROLLER = new SceneController();
-
 }
