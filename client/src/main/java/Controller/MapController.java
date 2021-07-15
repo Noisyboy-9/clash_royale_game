288d8499ce862c7504485ca73b7984bc8ff12dc4
nejadipour <nejadipour@gmail.com>
2021-07-15 13:01:14 +0430

    feat: fxml file ready for before game state

---
+++ client/src/main/java/Controller/MapController.java
@@ -0,0 +1,53 @@
+package Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.Group;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
+public class MapController
+{
+    @FXML
+    public void initialize()
+    {
+        
+    }
+
+    @FXML
+    private Group beforeGameState;
+
+    @FXML
+    private ImageView blackCover;
+
+    @FXML
+    private ImageView opponentBackground1;
+
+    @FXML
+    private ImageView playerBackground1;
+
+    @FXML
+    private ImageView opponentBackground2;
+
+    @FXML
+    private ImageView playerBackground2;
+
+    @FXML
+    private Text opponentName1;
+
+    @FXML
+    private Text playerName1;
+
+    @FXML
+    private Text opponentName2;
+
+    @FXML
+    private Text playerName2;
+
+    @FXML
+    void startGame(MouseEvent event)
+    {
+
+
+    }
+}
