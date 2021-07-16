a890b9b99a614e637a07e19e0e325399de63addf
sina shariati <sinashariati@sinas-MacBook-Pro.local>
2021-07-16 22:45:28 +0430

    Finished History Query Builder

--- client/src/main/java/Database/QueryBuilder/HistoryQueryBuilder.java
+++ client/src/main/java/Database/QueryBuilder/HistoryQueryBuilder.java
@@ -7,6 +7,9 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+/**
+ * The type History query builder.
+ */
 public class HistoryQueryBuilder {
     private final File historyDb;
 
@@ -14,6 +17,13 @@ public class HistoryQueryBuilder {
         this.historyDb = new File("client/src/main/java/Database/files/history.database.binary");
     }
 
+    /**
+     * Insert.
+     *
+     * @param user   the user
+     * @param result the result
+     * @throws IOException the io exception
+     */
     public void insert(User user, GameResult result) throws IOException {
         ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(this.historyDb, true));
 
@@ -23,21 +33,36 @@ public class HistoryQueryBuilder {
         outputStream.writeObject(userToResultMap);
     }
 
+    /**
+     * Read all array list.
+     *
+     * @param user the user
+     * @return the array list
+     * @throws IOException the io exception
+     */
     public ArrayList<GameResult> readAll(User user) throws IOException {
         if (this.historyDb.length() == 0) {
             return null;
         }
 
         ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(this.historyDb));
+        ArrayList<GameResult> results = new ArrayList<>();
         while (true) {
             try {
-                HashMap<User, GameResult> result = inputStream.readObject();
-
+                HashMap<User, GameResult> userToResultMap = (HashMap<User, GameResult>) inputStream.readObject();
+                for (User keyUser : userToResultMap.keySet()) {
+                    if (user.equals(keyUser)) {
+                        results.add(userToResultMap.get(keyUser));
+                    }
+                }
             } catch (ClassNotFoundException e) {
-                e.printStackTrace();
+                break;
             }
         }
+        if (results.size() == 0) {
+            return null;
+        }
 
-        return null;
+        return results;
     }
 }
