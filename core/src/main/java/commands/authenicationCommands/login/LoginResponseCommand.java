cd5f3155eb60cfd6453ca8519a451884520ddd30
sina shariati <sinashariati@sinas-MacBook-Pro.local>
2021-07-16 17:16:10 +0430

    Refactor Authenication Response commands to store the user
    the Authenication response commands should store the user.

--- core/src/main/java/commands/authenicationCommands/login/LoginResponseCommand.java
+++ core/src/main/java/commands/authenicationCommands/login/LoginResponseCommand.java
@@ -1,22 +1,36 @@
 package commands.authenicationCommands.login;
 
+import user.User;
+
 /**
  * The type Login response command.
  */
 public class LoginResponseCommand extends LoginCommand {
     private final boolean successful;
     private final String message;
+    private User user;
 
     /**
      * Instantiates a new Login response command.
      *
-     * @param username the username
-     * @param password the password
+     * @param user       the user
+     * @param successful the successful
+     * @param message    the message
      */
-    public LoginResponseCommand(String username, String password, boolean successful, String message) {
-        super(username, password);
+    public LoginResponseCommand(User user, boolean successful, String message) {
+        super(user.getUsername(), user.getPassword());
         this.successful = successful;
         this.message = message;
+        this.user = user;
+    }
+
+    /**
+     * Gets user.
+     *
+     * @return the user
+     */
+    public User getUser() {
+        return user;
     }
 
     /**
--- core/src/main/java/commands/authenicationCommands/register/RegisterResponseCommand.java
+++ core/src/main/java/commands/authenicationCommands/register/RegisterResponseCommand.java
@@ -1,22 +1,36 @@
 package commands.authenicationCommands.register;
 
+import user.User;
+
 /**
  * The type Register response command.
  */
 public class RegisterResponseCommand extends RegisterCommand {
     private final boolean successful;
     private final String message;
+    private final User user;
 
     /**
      * Instantiates a new Register response command.
      *
-     * @param username the username
-     * @param password the password
+     * @param user       the user
+     * @param successful the successful
+     * @param message    the message
      */
-    public RegisterResponseCommand(String username, String password, boolean successful, String message) {
-        super(username, password);
+    public RegisterResponseCommand(User user, boolean successful, String message) {
+        super(user.getUsername(), user.getPassword());
         this.successful = successful;
         this.message = message;
+        this.user = user;
+    }
+
+    /**
+     * Gets user.
+     *
+     * @return the user
+     */
+    public User getUser() {
+        return user;
     }
 
     /**
