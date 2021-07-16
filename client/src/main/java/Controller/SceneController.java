3a5ec5ace746e7ad46aa9d8bffb7caec7cc5bc39
sina shariati <sinashariati@sinas-MacBook-Pro.local>
2021-07-16 13:02:34 +0430

    Implemented singleton pattern for Connector class
    the connector class needs to be accessible from all places of the code.
    So it must implement the singleton pattern, but the problem is that it's controller
    have 3 arguments, so had to come up with another function called connect,
    which will be called at project bootstrap, and then in all other parts of the
    project if we call it we will get Excepiton, for getting the instance
    of the connection client must use the getInstance() method.

--- client/src/main/java/Controller/SceneController.java
+++ client/src/main/java/Controller/SceneController.java
@@ -1,10 +1,13 @@
 package Controller;
 
+import connector.Connector;
+import exceptions.AlreadyConnectedToServerException;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 
 import java.io.IOException;
+import java.net.Socket;
 import java.net.URL;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
@@ -20,7 +23,18 @@ public class SceneController {
 
     public SceneController() {
         this.menus = new HashMap<>();
+        try {
+            this.connectToServer();
+        } catch (IOException | AlreadyConnectedToServerException exception) {
+            exception.printStackTrace();
+        }
+    }
 
+    private void connectToServer() throws IOException, AlreadyConnectedToServerException {
+        Socket socket = new Socket("localhost", 8080);
+
+//        create a instance of the connection, and then fetch with the getInstance() method.
+        Connector connector = Connector.connect(socket, socket.getInputStream(), socket.getOutputStream());
     }
 
     public void showScene(String sceneName) {
@@ -79,5 +93,4 @@ public class SceneController {
 //                create a directory stream for windows
         return Files.newDirectoryStream(Paths.get(Controller.PATH + "Menu"));
     }
-
 }
