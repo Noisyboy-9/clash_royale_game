b047961639756be8222c6159555b1cfed991ddb3
sina shariati <sinashariati@sinas-MacBook-Pro.local>
2021-07-16 17:23:40 +0430

    Refactor Authenication Response class to store User
    the authenication response commands should store a user inside of it
    to be able to send it through the socket and it's Input & Output streams
    to the client, so the client can store it, and use it as it wishes.
    chagned the classes to have two classes, one for when their response
    is sucessful, and one for when the response is not sucessful.
    when response is sucessful it will store a user.
    if it isn't sucessful it will have a null pointer in it's user field.

--- core/src/main/java/commands/authenicationCommands/login/LoginResponseCommand.java
+++ core/src/main/java/commands/authenicationCommands/login/LoginResponseCommand.java
@@ -1,22 +1,42 @@
 package commands.authenicationCommands.login;
 
+import user.User;
+
 /**
  * The type Login response command.
  */
 public class LoginResponseCommand extends LoginCommand {
     private final boolean successful;
     private final String message;
+    private final User user;
 
     /**
      * Instantiates a new Login response command.
+     * for the situation that a login is successfully made.
+     *
+     * @param user    the user
+     * @param message the message
+     */
+    public LoginResponseCommand(User user, String message) {
+        super(user.getUsername(), user.getPassword());
+        this.user = user;
+        this.successful = true;
+        this.message = message;
+    }
+
+    /**
+     * Instantiates a new Login response command.
+     * for the situation that the Login is not successful;
      *
      * @param username the username
      * @param password the password
+     * @param message  the message
      */
-    public LoginResponseCommand(String username, String password, boolean successful, String message) {
+    public LoginResponseCommand(String username, String password, String message) {
         super(username, password);
-        this.successful = successful;
         this.message = message;
+        this.user = null;
+        this.successful = false;
     }
 
     /**
--- core/src/main/java/commands/authenicationCommands/register/RegisterResponseCommand.java
+++ core/src/main/java/commands/authenicationCommands/register/RegisterResponseCommand.java
@@ -1,21 +1,41 @@
 package commands.authenicationCommands.register;
 
+import user.User;
+
 /**
  * The type Register response command.
  */
 public class RegisterResponseCommand extends RegisterCommand {
     private final boolean successful;
     private final String message;
+    private final User user;
 
     /**
      * Instantiates a new Register response command.
+     * for the situation that an unsuccessful register has been made
      *
      * @param username the username
      * @param password the password
+     * @param message  the message
      */
-    public RegisterResponseCommand(String username, String password, boolean successful, String message) {
+    public RegisterResponseCommand(String username, String password, String message) {
         super(username, password);
-        this.successful = successful;
+        this.user = null;
+        this.successful = false;
+        this.message = message;
+    }
+
+    /**
+     * Instantiates a new Register response command.
+     * for the situation that a successful register has been made.
+     *
+     * @param user    the user
+     * @param message the message
+     */
+    public RegisterResponseCommand(User user, String message) {
+        super(user.getUsername(), user.getPassword());
+        this.user = user;
+        this.successful = true;
         this.message = message;
     }
 
