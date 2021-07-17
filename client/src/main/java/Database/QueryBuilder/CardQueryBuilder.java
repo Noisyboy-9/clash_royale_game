d034b3054d7049016a4e1c1b30fcb4fec6106fe2
sina shariati <sinashariati@sinas-MacBook-Pro.local>
2021-07-17 16:49:51 +0430

    Fixed problem in Card Query Builder

--- client/src/main/java/Database/QueryBuilder/CardQueryBuilder.java
+++ client/src/main/java/Database/QueryBuilder/CardQueryBuilder.java
@@ -14,14 +14,20 @@ import java.util.Objects;
 /**
  * The type Card query builder.
  */
-public class CardQueryBuilder
-{
+public class CardQueryBuilder {
     private static CardQueryBuilder singletonInstance = null;
     private final File cardsDb;
+    private ObjectInputStream reader;
+    private ObjectOutputStream writer;
 
-    private CardQueryBuilder()
-    {
+    private CardQueryBuilder() {
         this.cardsDb = new File("client/src/main/java/Database/files/cards.database.binary");
+        try {
+            this.reader = new ObjectInputStream(new FileInputStream(this.cardsDb));
+            this.writer = new ObjectOutputStream(new FileOutputStream(this.cardsDb));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -30,61 +36,38 @@ public class CardQueryBuilder
      * @param user       the user
      * @param battleDeck the battle deck
      */
-    public void updatePlayerCards(User user, GridPane battleDeck)
-    {
+    public void updatePlayerCards(User user, GridPane battleDeck) {
+        HashMap<User, HashMap<Integer, String>> userToCardsMap = null;
 
-        try(
-                FileOutputStream fos = new FileOutputStream(this.cardsDb);
-                ObjectOutputStream outputStream = new ObjectOutputStream(fos))
-        {
-            HashMap<User, HashMap<Integer, String>> userToCardsMap = null;
-
-            if (cardsDb.length() != 0)
-            {
-                FileInputStream fis = new FileInputStream(this.cardsDb);
-                ObjectInputStream inputStream = new ObjectInputStream(fis);
-
-                try
-                {
-                    userToCardsMap = (HashMap<User, HashMap<Integer, String>>) inputStream.readObject();
-                }
-                catch (EOFException e)
-                {
-                    userToCardsMap = null;
-                }
-
-                fis.close();
-                inputStream.close();
-
+        if (cardsDb.length() != 0) {
+            try {
+                userToCardsMap = (HashMap<User, HashMap<Integer, String>>) this.reader.readObject();
+            } catch (EOFException e) {
+                userToCardsMap = null;
+            } catch (IOException | ClassNotFoundException ioException) {
+                ioException.printStackTrace();
             }
-
-
-            if (userToCardsMap == null)
-            {
-                userToCardsMap = new HashMap<>();
-            }
-
-            if (userToCardsMap.containsKey(user))
-            {
-                userToCardsMap.replace(user, getUrls(battleDeck));
-
-            }
-            else
-            {
-                userToCardsMap.put(user, getUrls(battleDeck));
-            }
-
-            outputStream.writeObject(userToCardsMap);
-
         }
-        catch (IOException | ClassNotFoundException e)
-        {
-            e.printStackTrace();
+
+        if (userToCardsMap == null) {
+            userToCardsMap = new HashMap<>();
+        }
+
+        if (userToCardsMap.containsKey(user)) {
+            userToCardsMap.replace(user, getUrls(battleDeck));
+
+        } else {
+            userToCardsMap.put(user, getUrls(battleDeck));
+        }
+
+        try {
+            this.writer.writeObject(userToCardsMap);
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
         }
 
         Controller.SCENE_CONTROLLER.removeScene("Menu/BattleDeckMenu.fxml");
         Controller.SCENE_CONTROLLER.removeScene("Menu/ProfileMenu.fxml");
-
     }
 
     /**
@@ -93,45 +76,22 @@ public class CardQueryBuilder
      * @param user the user
      * @return the hash map
      */
-    public HashMap<Integer, String> loadCards(User user)
-    {
-        if (cardsDb.length() == 0)
-        {
+    public HashMap<Integer, String> loadCards(User user) {
+        if (cardsDb.length() == 0) {
             return null;
         }
 
-        ObjectInputStream inputStream = null;
-        try
-        {
-            inputStream = new ObjectInputStream(new FileInputStream(this.cardsDb));
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
+        try {
+            HashMap<User, HashMap<Integer, String>> userToCardsMap = (HashMap<User, HashMap<Integer, String>>) this.reader.readObject();
 
-        }
-
-        try
-        {
-            if (inputStream != null)
-            {
-                HashMap<User, HashMap<Integer, String>> userToCardsMap = (HashMap<User, HashMap<Integer, String>>) inputStream.readObject();
-
-                if (userToCardsMap != null && userToCardsMap.containsKey(user))
-                {
-                    return userToCardsMap.get(user);
-                }
-
+            if (userToCardsMap != null && userToCardsMap.containsKey(user)) {
+                return userToCardsMap.get(user);
             }
-
-        }
-        catch (ClassNotFoundException | IOException e)
-        {
+        } catch (ClassNotFoundException | IOException e) {
             e.printStackTrace();
         }
 
         return null;
-
     }
 
     /**
@@ -147,26 +107,19 @@ public class CardQueryBuilder
         return singletonInstance;
     }
 
-    private HashMap<Integer, String> getUrls(GridPane battleDeck)
-    {
+    private HashMap<Integer, String> getUrls(GridPane battleDeck) {
         HashMap<Integer, String> urls = new HashMap<>();
 
-        for (Node node : battleDeck.getChildren())
-        {
-            if (node instanceof ImageView)
-            {
+        for (Node node : battleDeck.getChildren()) {
+            if (node instanceof ImageView) {
                 String url = ((ImageView) node).getImage().getUrl();
-                if (!url.contains("card_exir"))
-                {
+                if (!url.contains("card_exir")) {
                     int index = battleDeck.getChildren().indexOf(node);
-                    String elixirCount = ((Text)battleDeck.getChildren().get(index + 16)).getText();
+                    String elixirCount = ((Text) battleDeck.getChildren().get(index + 16)).getText();
 
                     urls.put(index, url + "->" + elixirCount);
-
                 }
-
             }
-
         }
 
         return urls;
