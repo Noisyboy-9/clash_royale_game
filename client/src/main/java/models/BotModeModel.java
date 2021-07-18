dabf93502a209b797e50fbbc9d2d13ebe8fc98e0
nejadipour <nejadipour@gmail.com>
2021-07-18 19:09:43 +0430

    feat: constructor completed

--- client/src/main/java/models/BotModeModel.java
+++ client/src/main/java/models/BotModeModel.java
@@ -1,6 +1,9 @@
 package models;
 
 import cards.Card;
+import globals.UserData;
+import towers.KingTower;
+import towers.QueenTower;
 import towers.Tower;
 
 import java.util.ArrayList;
@@ -19,6 +22,31 @@ public class BotModeModel
     private ArrayList<Card> playerBattleCards;
     private ArrayList<Card> playerComingCards;
     private int playerElixirCount;
+
+    public BotModeModel(ArrayList<Card> botAllCards, ArrayList<Card> botBattleCards, ArrayList<Card> botComingCards,
+                        ArrayList<Card> playerAllCards, ArrayList<Card> playerBattleCards, ArrayList<Card> playerComingCards) {
+        this.botTowers = new ArrayList<>();
+        this.botAllCards = botAllCards;
+        this.botInMapCards = new ArrayList<>();
+        this.botBattleCards = botBattleCards;
+        this.botComingCards = botComingCards;
+        this.botElixirCount = 4;
+        this.playerTowers = new ArrayList<>();
+        this.playerAllCards = playerAllCards;
+        this.playerInMapCards = new ArrayList<>();
+        this.playerBattleCards = playerBattleCards;
+        this.playerComingCards = playerComingCards;
+        this.playerElixirCount = 4;
+
+        this.botTowers.add(QueenTower.create(UserData.bot));
+        this.botTowers.add(KingTower.create(UserData.bot));
+        this.botTowers.add(QueenTower.create(UserData.bot));
+
+        this.playerTowers.add(QueenTower.create(UserData.user));
+        this.playerTowers.add(KingTower.create(UserData.user));
+        this.playerTowers.add(QueenTower.create(UserData.user));
+
+    }
 
     public ArrayList<Tower> getBotTowers() {
         return botTowers;
