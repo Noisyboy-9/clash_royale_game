5dc19861e22f4fde9bffd98452c644b914532f68
sina shariati <sinashariati@sinas-MacBook-Pro.local>
2021-07-20 13:04:26 +0430

    Change CanKnowItsExactGameStateCommandTrait to contain new checks for card command

--- core/src/main/java/commands/traits/CanKnowItsExactGameStateCommandTrait.java
+++ core/src/main/java/commands/traits/CanKnowItsExactGameStateCommandTrait.java
@@ -1,61 +1,31 @@
 package commands.traits;
 
 import commands.gameStateCommands.GameStateCommand;
+import commands.gameStateCommands.cardCommands.CardAddedCommand;
+import commands.gameStateCommands.cardCommands.CardCommand;
+import commands.gameStateCommands.cardCommands.CardDeletedCommand;
 import commands.gameStateCommands.gameBonusCommands.CrownCountChangeCommand;
 import commands.gameStateCommands.gameBonusCommands.GameBonusCommands;
 import commands.gameStateCommands.gameTimeCommands.GameDurationFinished;
 import commands.gameStateCommands.gameTimeCommands.GameFinishedCommand;
 import commands.gameStateCommands.gameTimeCommands.GameStartCommand;
 import commands.gameStateCommands.gameTimeCommands.GameTimeCommand;
-import commands.gameStateCommands.spellCommands.SpellAddedCommand;
-import commands.gameStateCommands.spellCommands.SpellCommand;
-import commands.gameStateCommands.spellCommands.SpellDurationFinishedCommand;
-import commands.gameStateCommands.towerCommands.TowerActiveCommand;
-import commands.gameStateCommands.towerCommands.TowerCommand;
-import commands.gameStateCommands.towerCommands.TowerDestroyedCommand;
-import commands.gameStateCommands.troopCommands.TroopAddedCommand;
-import commands.gameStateCommands.troopCommands.TroopCommand;
-import commands.gameStateCommands.troopCommands.TroopKilledCommand;
 
 public interface CanKnowItsExactGameStateCommandTrait {
     default boolean isGameStateCommand() {
         return this instanceof GameStateCommand;
     }
 
-    default boolean isTroopCommand() {
-        return this instanceof TroopCommand;
+    default boolean isCardCommand() {
+        return this instanceof CardCommand;
     }
 
-    default boolean isTroopAddedCommand() {
-        return this instanceof TroopAddedCommand;
+    default boolean isCardAddedCommand() {
+        return this instanceof CardAddedCommand;
     }
 
-    default boolean isTroopKilledCommand() {
-        return this instanceof TroopKilledCommand;
-    }
-
-    default boolean isTowerCommand() {
-        return this instanceof TowerCommand;
-    }
-
-    default boolean isTowerActiveCommand() {
-        return this instanceof TowerActiveCommand;
-    }
-
-    default boolean isTowerDestroyedCommand() {
-        return this instanceof TowerDestroyedCommand;
-    }
-
-    default boolean isSpellCommand() {
-        return this instanceof SpellCommand;
-    }
-
-    default boolean isSpellAddedCommand() {
-        return this instanceof SpellAddedCommand;
-    }
-
-    default boolean isSpellDurationFinishedCommand() {
-        return this instanceof SpellDurationFinishedCommand;
+    default boolean isCardDeletedCommand() {
+        return this instanceof CardDeletedCommand;
     }
 
     default boolean isGameTimeCommand() {
