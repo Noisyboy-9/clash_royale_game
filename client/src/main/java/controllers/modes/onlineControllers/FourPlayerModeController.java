7b5cefce2e6796261df73b4597b0c523fd69975b
sina shariati <sinashariati@sinas-MacBook-Pro.local>
2021-07-21 21:32:31 +0430

    Finished four player mode controller

--- client/src/main/java/controllers/modes/onlineControllers/FourPlayerModeController.java
+++ client/src/main/java/controllers/modes/onlineControllers/FourPlayerModeController.java
@@ -1,4 +1,64 @@
 package controllers.modes.onlineControllers;
 
+import controllers.modes.runnables.*;
+import models.OnlineModeModel;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class FourPlayerModeController extends OnlineController {
+    /**
+     * Instantiates a new Bot controller.
+     *
+     * @param model the model
+     */
+    public FourPlayerModeController(OnlineModeModel model) {
+        super(model);
+    }
+
+    @Override
+    protected void setTimer() {
+        Timer timer = new Timer();
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                ExecutorService service = Executors.newCachedThreadPool();
+                service.execute(
+                        new HandleTowersRunnable(FourPlayerModeController.super.model, FourPlayerModeController.this)
+                );
+
+                service.execute(
+                        new HandleTroopsRunnable(FourPlayerModeController.super.model, FourPlayerModeController.this)
+                );
+
+                service.execute(
+                        new HandleSpellsRunnable(FourPlayerModeController.super.model, FourPlayerModeController.this)
+                );
+
+                service.execute(
+                        new HandleBuildingRunnable(FourPlayerModeController.super.model, FourPlayerModeController.this)
+                );
+
+                service.execute(
+                        new HandleElixirsCountRunnable(FourPlayerModeController.super.model, FourPlayerModeController.this)
+                );
+
+//                after all these threads are finished: render screen again.
+                service.shutdown();
+                try {
+                    service.awaitTermination(FourPlayerModeController.super.eachFrameDuration, TimeUnit.MILLISECONDS);
+                    FourPlayerModeController.super.render();
+                    FourPlayerModeController.this.reduceFrameRemainingCount();
+                } catch (InterruptedException e) {
+                    System.out.println("The time of logical threads has exceeded the frame each frame duration");
+                    System.out.println("maybe it is better to use a lower frame per second");
+                }
+            }
+        };
+
+        timer.schedule(timerTask, 0, this.eachFrameDuration);
+    }
 }
