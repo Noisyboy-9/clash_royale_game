431f4bb203355ef07d23afe2e977f0f6a65a2ebc
nejadipour <nejadipour@gmail.com>
2021-07-23 00:26:52 +0430

    feat: 2 player mode method added

--- client/src/main/java/controllers/menus/MainMenuController.java
+++ client/src/main/java/controllers/menus/MainMenuController.java
@@ -1,11 +1,19 @@
 package controllers.menus;
 
+import cards.Card;
+import commands.gameStateCommands.gameTimeCommands.GameStartCommand;
 import controllers.Controller;
+import controllers.connector.Connector;
 import globals.GlobalData;
 import javafx.fxml.FXML;
+import javafx.geometry.Point2D;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
+import models.OnlineModeModel;
+import towers.Tower;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class MainMenuController extends MenuController {
@@ -25,7 +33,32 @@ public class MainMenuController extends MenuController {
 
     @FXML
     void twoPlayerMode(MouseEvent event) {
-        // TODO: ۱۴/۰۷/۲۰۲۱ show wait scene and send command to server
+        ArrayList<Tower> enemyTowers = new ArrayList<>();
+        ArrayList<Tower> friendlyTowers = new ArrayList<>();
+
+        try {
+            GameStartCommand command = (GameStartCommand) Connector.getInstance().getResponse().readObject();
+            enemyTowers = command.getEnemyTowers();
+            friendlyTowers = command.getFriendlyTowers();
+            setPositions(enemyTowers);
+            setPositions(friendlyTowers);
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        ArrayList<Card> playerAllCards = GlobalData.createCards(GlobalData.user);
+        ArrayList<Card> playerBattleCards = new ArrayList<>(playerAllCards.subList(0, 4));
+
+        
+
+
+
+
         Controller.SCENE_CONTROLLER.showScene("Menu/WaitingPage.fxml");
     }
 
@@ -41,4 +74,22 @@ public class MainMenuController extends MenuController {
         Controller.SCENE_CONTROLLER.showScene("Menu/LoginRegisterPage.fxml");
 
     }
+
+
+    private void setPositions(ArrayList<Tower> towers) {
+        towers.get(0).setPosition(new Point2D(6, 29));
+        towers.get(1).setPosition(new Point2D(17, 29));
+
+        if (towers.size() > 3) {
+            towers.get(2).setPosition(new Point2D(9, 32));
+            towers.get(3).setPosition(new Point2D(14, 32));
+        }
+        else {
+            towers.get(2).setPosition(new Point2D(9, 34));
+
+        }
+
+    }
+
+
 }
