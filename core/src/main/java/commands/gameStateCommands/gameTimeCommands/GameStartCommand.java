455ae032adcf3c0bec3e492a1c1e373a0e595b5b
sina shariati <sinashariati@sinas-MacBook-Pro.local>
2021-07-22 11:58:38 +0430

    Add friendly and enemy towers to GameStartCommand

--- core/src/main/java/commands/gameStateCommands/gameTimeCommands/GameStartCommand.java
+++ core/src/main/java/commands/gameStateCommands/gameTimeCommands/GameStartCommand.java
@@ -1,6 +1,7 @@
 package commands.gameStateCommands.gameTimeCommands;
 
 import commands.gameStateCommands.enums.GameTypeEnum;
+import towers.Tower;
 import user.User;
 
 import java.util.ArrayList;
@@ -9,17 +10,41 @@ import java.util.ArrayList;
  * The type Game start command.
  */
 public class GameStartCommand extends GameTimeCommand {
-    private final ArrayList<User> users;
+    private final ArrayList<User> receivers;
+    private final ArrayList<Tower> friendlyTowers;
+    private final ArrayList<Tower> enemyTowers;
 
     /**
      * Instantiates a new Game time command.
      *
-     * @param gameType the game type
-     * @param users    the users
+     * @param gameType       the game type
+     * @param receivers      the users
+     * @param friendlyTowers the friendly towers
+     * @param enemyTowers    the enemy towers
      */
-    public GameStartCommand(GameTypeEnum gameType, ArrayList<User> users) {
+    public GameStartCommand(GameTypeEnum gameType, ArrayList<User> receivers, ArrayList<Tower> friendlyTowers, ArrayList<Tower> enemyTowers) {
         super(gameType);
-        this.users = users;
+        this.receivers = receivers;
+        this.friendlyTowers = friendlyTowers;
+        this.enemyTowers = enemyTowers;
+    }
+
+    /**
+     * Gets friendly towers.
+     *
+     * @return the friendly towers
+     */
+    public ArrayList<Tower> getFriendlyTowers() {
+        return friendlyTowers;
+    }
+
+    /**
+     * Gets enemy towers.
+     *
+     * @return the enemy towers
+     */
+    public ArrayList<Tower> getEnemyTowers() {
+        return enemyTowers;
     }
 
     /**
@@ -27,7 +52,7 @@ public class GameStartCommand extends GameTimeCommand {
      *
      * @return the users
      */
-    public ArrayList<User> getUsers() {
-        return users;
+    public ArrayList<User> getReceivers() {
+        return receivers;
     }
 }
